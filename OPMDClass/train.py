f822e6c436ca8cc1250075d3ebef3c675a68c202
Taran Kaur <dsmf8385@leeds.ac.uk>
2026-04-24 14:17:31 +0100

    Fine-tuning parameters

--- OPMDClass/train.py
+++ OPMDClass/train.py
@@ -9,7 +9,7 @@ from submission.model import DentalClassifier
 
 def fit(model, train_loader, val_loader, epochs = 20):
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.classifier.parameters(), lr = 2e-4)
+    optimizer = optim.Adam(model.classifier.parameters(), lr = 1e-4)
 
     least_val_loss = float("inf")
 
